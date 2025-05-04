1f7f8987c83aca3cecb2c0a1761c994a7197ebf3
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-05-04 13:47:37 +0600

    feat: update error handling in up function

--- src/operations/navigation/up.js
+++ src/operations/navigation/up.js
@@ -4,11 +4,10 @@ import os from 'node:os';
 const rootDir = path.parse(os.homedir()).root;
 
 export const up = (args, state) => {
-  const parentDir = path.dirname(state.currentDir);
-
-  if (!parentDir.startsWith(rootDir)) {
+  if (state.currentDir === rootDir) {
     throw new Error('Root directory is reached!');
   }
 
+  const parentDir = path.dirname(state.currentDir);
   state.currentDir = parentDir;
 };
