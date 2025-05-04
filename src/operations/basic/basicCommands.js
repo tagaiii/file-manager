db5ba916eb3bf517362f2d7fdd9521a422c6aeae
Kamalov Tagaibek <tagai.nambavan@gmail.com>
2025-05-04 16:17:50 +0600

    feat: implement mkdir function

--- src/operations/basic/basicCommands.js
+++ src/operations/basic/basicCommands.js
@@ -1,9 +1,11 @@
 import { cat } from './cat.js';
 import { add } from './add.js';
+import { mkdir } from './mkdir.js';
 
 const basicCommands = new Map();
 
 basicCommands.set('cat', cat);
 basicCommands.set('add', add);
+basicCommands.set('mkdir', mkdir);
 
 export default basicCommands;
---
+++ src/operations/basic/mkdir.js
@@ -0,0 +1,15 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { colors } from '../../utils/colors.js';
+
+export const mkdir = async (args, state) => {
+  const dirName = args.join(' ');
+  const dirPath = path.resolve(state.currentDir, dirName);
+
+  try {
+    await fs.mkdir(dirPath);
+    console.log(colors.green(`Directory ${dirName} is successfully created!`));
+  } catch {
+    throw new Error(`Directory ${dirName} already exists!`);
+  }
+};
